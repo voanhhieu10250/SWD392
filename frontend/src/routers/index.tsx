90fb25477da1d331d1a2b9a145b116c1b452adc4
dattienle <tiendatle360@gmail.com>
2024-03-13 11:37:56 +0700

    làm pre-order cho trang profile

--- frontend/src/routers/index.tsx
+++ frontend/src/routers/index.tsx
@@ -1,28 +1,23 @@
-import { FC, ComponentType } from 'react';
-import { Navigate, useRoutes, useLocation } from 'react-router-dom';
-import Login from '../pages/auth/Login';
-import MainLayout from '~/layouts/main';
-import Main from '~/pages/components/main';
+import { Navigate, useRoutes } from 'react-router-dom'
+import Login from '../pages/auth/Login'
+import MainLayout from '~/layouts/main'
+import Main from '~/pages/components/main'
 import Search from '../pages/components/search/Search'
+import ProfileUser from '~/pages/components/profile'
 
 const Router = () => {
   return useRoutes([
-
     // main layouut
     {
       path: '/',
       element: <MainLayout />,
-      children: [
-        { element: <Main />, index: true },
-        
-      ],
+      children: [{ element: <Main />, index: true }]
     },
     { path: 'login', element: <Login /> },
     { path: 'search', element: <Search /> },
-    { path: '*', element: <Navigate to="/404" replace /> },
+    { path: 'profile/:userId', element: <ProfileUser /> },
+    { path: '*', element: <Navigate to='/404' replace /> }
+  ])
+}
 
-  ]
-  );
-};
-
-export default Router;
+export default Router
