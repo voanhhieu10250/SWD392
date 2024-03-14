6b82c3a35974c80b7458aa7abb10a8fae8be4644
dattienle <tiendatle360@gmail.com>
2024-03-14 10:58:40 +0700

    login page

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,36 +1,73 @@
-import React, { FC } from 'react';
+import React, { FC } from 'react'
 
 const Login: FC = () => {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Đăng nhập vào tài khoản của bạn
-                    </h2>
-                </div>
-                <form className="mt-8 space-y-6" action="#" method="POST">
-                    <input type="hidden" name="remember" value="true" />
-                    <div className="rounded-md shadow-sm -space-y-px">
-                        <div>
-                            <label htmlFor="email-address" className="sr-only">Email</label>
-                            <input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email" />
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="sr-only">Mật khẩu</label>
-                            <input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Mật khẩu" />
-                        </div>
-                    </div>
-
-                    <div>
-                        <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Đăng nhập
-                        </button>
-                    </div>
-                </form>
-            </div>
+  return (
+    <div className='container mx-auto'>
+      <div className='flex justify-center px-6 my-12'>
+        <div className='w-full xl:w-3/4 lg:w-11/12 flex'>
+          <div
+            className='w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg'
+            style={{
+              backgroundImage: "url('https://source.unsplash.com/K4mSJ7kc0As/600x800')"
+            }}
+          ></div>
+          <div className='w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none'>
+            <h3 className='pt-4 text-2xl text-center'>Welcome Back!</h3>
+            <form className='px-8 pt-6 pb-8 mb-4 bg-white rounded'>
+              <div className='mb-4'>
+                <label className='block mb-2 text-sm font-bold text-gray-700' htmlFor='username'>
+                  Username
+                </label>
+                <input
+                  className='w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline'
+                  id='username'
+                  type='text'
+                  placeholder='Username'
+                />
+              </div>
+              <div className='mb-4'>
+                <label className='block mb-2 text-sm font-bold text-gray-700' htmlFor='password'>
+                  Password
+                </label>
+                <input
+                  className='w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline'
+                  id='password'
+                  type='password'
+                  placeholder='*************'
+                />
+                <p className='text-xs italic text-red-500'>Please choose a password.</p>
+              </div>
+              <div className='mb-4'>
+                <input className='mr-2 leading-tight' type='checkbox' id='checkbox_id' />
+                <label className='text-sm' htmlFor='checkbox_id'>
+                  Remember Me
+                </label>
+              </div>
+              <div className='mb-6 text-center'>
+                <button
+                  className='w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline'
+                  type='button'
+                >
+                  Sign In
+                </button>
+              </div>
+              <hr className='mb-6 border-t' />
+              <div className='text-center'>
+                <a className='inline-block text-sm text-blue-500 align-baseline hover:text-blue-800' href='#'>
+                  Create an Account!
+                </a>
+              </div>
+              <div className='text-center'>
+                <a className='inline-block text-sm text-blue-500 align-baseline hover:text-blue-800' href='#'>
+                  Forgot Password?
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
-    );
-};
+      </div>
+    </div>
+  )
+}
 
-export default Login;
+export default Login
