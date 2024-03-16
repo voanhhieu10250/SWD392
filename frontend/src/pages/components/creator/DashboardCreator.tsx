eae320d82600f0013f1798bda022c53aa07990ba
dattienle <tiendatle360@gmail.com>
2024-03-16 19:04:57 +0700

    xong page lịch sử mua package, lịch sử mua bán physical art

--- frontend/src/pages/components/creator/DashboardCreator.tsx
+++ frontend/src/pages/components/creator/DashboardCreator.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import DataTable from './DataTable'
 import Detail from './Detail'
+import HistoryBuyPackage from './HistoryBuyPackage'
+import TransactionHistory from './HistoryPhysicalArt'
 
 export default function DashboardCreator() {
   const [currentTab, setCurrentTab] = useState('dashboard')
@@ -22,15 +24,11 @@ export default function DashboardCreator() {
         )
       case 'details':
         return selectedUser && <Detail user={selectedUser} onBack={() => setCurrentTab('dashboard')} />
-      case 'inbox':
-        return <div>Inbox content goes here</div>
-      case 'users':
-        return <div>Users content goes here</div>
-      case 'products':
-        return <div>Products content goes here</div>
-      case 'signIn':
-        return <div>Sign In content goes here</div>
-      case 'signUp':
+      case 'packageHistory':
+        return <HistoryBuyPackage />
+      case 'historyPhysicalArt':
+        return <TransactionHistory />
+      case 'logout':
         return <div>Sign Up content goes here</div>
       default:
         return <div>Please select a tab</div>
@@ -110,6 +108,7 @@ export default function DashboardCreator() {
               <a
                 href='#'
                 className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
+                onClick={() => handleTabClick('packageHistory')}
               >
                 <svg
                   className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
@@ -120,16 +119,14 @@ export default function DashboardCreator() {
                 >
                   <path d='m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z' />
                 </svg>
-                <span className='flex-1 ms-3 whitespace-nowrap'>Inbox</span>
-                <span className='inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300'>
-                  3
-                </span>
+                <span className='flex-1 ms-3 whitespace-nowrap'>History Buy Package</span>
               </a>
             </li>
             <li>
               <a
                 href='#'
                 className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
+                onClick={() => handleTabClick('historyPhysicalArt')}
               >
                 <svg
                   className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
@@ -140,26 +137,10 @@ export default function DashboardCreator() {
                 >
                   <path d='M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z' />
                 </svg>
-                <span className='flex-1 ms-3 whitespace-nowrap'>Users</span>
+                <span className='flex-1 ms-3 whitespace-nowrap'>History Physical Art</span>
               </a>
             </li>
-            <li>
-              <a
-                href='#'
-                className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group'
-              >
-                <svg
-                  className='flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white'
-                  aria-hidden='true'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='currentColor'
-                  viewBox='0 0 18 20'
-                >
-                  <path d='M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z' />
-                </svg>
-                <span className='flex-1 ms-3 whitespace-nowrap'>Products</span>
-              </a>
-            </li>
+
             <li>
               <a
                 href='#'
@@ -190,7 +171,7 @@ export default function DashboardCreator() {
       <div className='p-4 sm:ml-64'>
         {/* Content */}
         <div className='p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700'>
-          <div className='flex items-center justify-center  rounded bg-gray-50 dark:bg-gray-800'>{renderContent()}</div>
+          <div className='flex items-center justify-center rounded bg-gray-50 dark:bg-gray-800'>{renderContent()}</div>
         </div>
       </div>
     </div>
