c49e1ff02a172a5f29fb3b47a5a84dc1fbf8f6f5
LyDuy284 <lyhieuduy9190@gmail.com>
2024-03-17 13:42:05 +0700

    fix link api upload art

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -77,6 +77,7 @@ export const router = createBrowserRouter([
         // element: <DigitalArtPage />
         element: <PhysicalArtPage />
       },
+      
       {
         path: 'notification',
         element: <Notification />
