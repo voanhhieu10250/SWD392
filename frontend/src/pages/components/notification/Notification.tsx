fe089b0e961c29d6a68ad451163929949444467c
Le Tien Dat <tiendatle360@gmail.com>
2024-03-17 15:23:33 +0700

    fix if else mua package

--- frontend/src/pages/components/notification/Notification.tsx
+++ frontend/src/pages/components/notification/Notification.tsx
@@ -19,7 +19,7 @@ export default function Notification() {
               <p className='text-gray-600 my-2'>Thank you for completing your secure online payment.</p>
               <p>Have a great day!</p>
               <div className='py-10 text-center'>
-                <a href='/package' className='px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3'>
+                <a href='/payment' className='px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3'>
                   GO BACK
                 </a>
               </div>
@@ -35,7 +35,7 @@ export default function Notification() {
               <h3 className='md:text-2xl text-base text-gray-900 font-semibold text-center'>Payment Failed!</h3>
               <p className='text-gray-600 my-2'>Please try your payment again.</p>
               <div className='py-10 text-center'>
-                <a href='/package' className='px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3'>
+                <a href='/payment' className='px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3'>
                   TRY AGAIN
                 </a>
               </div>
