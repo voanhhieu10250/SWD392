563c014b1ff41a5fab32117fa2e71587e3bf7ea0
HieuNghia0000 <fakenamemc1@gmail.com>
2024-03-17 20:30:02 +0700

    update FE ActivitiLog

--- frontend/src/components/ArtDetailPage/ActivitiLogDialog.tsx
+++ frontend/src/components/ArtDetailPage/ActivitiLogDialog.tsx
@@ -2,8 +2,25 @@ import { FaAngleRight } from 'react-icons/fa'
 import { RiRepeatFill } from 'react-icons/ri'
 import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
 import ActivityLog from './ActivityLog'
+import {ResellTransaction, ResponseObj} from "~/types";
+import {useQuery} from "react-query";
+import {format} from 'date-fns';
+
+const fetchCurrentOwner = async () => {
+  const res = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/resell-transaction/current-owner`)
+  const data = (await res.json()) as ResponseObj<ResellTransaction>
+  return data.data
+}
 
 const ActivitiLogDialog = () => {
+
+  const {data} = useQuery<ResellTransaction | Error>('currentOwner',() => fetchCurrentOwner());
+  console.log(data)
+
+  const formatDate = (date: Date): string => {
+    return format(date, 'MMM d, yyyy');
+  };
+
   return (
     <Dialog>
       <DialogTrigger className='w-full'>
@@ -11,45 +28,47 @@ const ActivitiLogDialog = () => {
           <div className='flex justify-center mb-2'>
             <span className='font-bold'>Activity Log</span>
           </div>
-          <div className='flex justify-between'>
-            <div className='inline-flex'>
-              <div>
-                <img
-                  src='https://i.pinimg.com/236x/db/c4/f7/dbc4f7f26f83a1cedc0aa9523550ff26.jpg'
-                  alt='Avatar'
-                  width={50}
-                  className='rounded-lg'
-                />
+          {data && (
+            <div className='flex justify-between'>
+              <div className='inline-flex'>
+                <div>
+                  <img
+                    src={data.avatar || 'https://miro.medium.com/v2/resize:fit:640/format:webp/1*W35QUSvGpcLuxPo3SRTH4w.png'}
+                    alt='Avatar'
+                    width={50}
+                    className='rounded-lg'
+                  />
+                </div>
+                <div className='ml-2 mr-4 text-left'>
+                  <p className=''>Current Owner</p>
+                  <p className='font-bold'>{data.buyerUser.username}</p>
+                </div>
               </div>
-              <div className='ml-2 mr-4 text-left'>
-                <p className=''>Current Owner</p>
-                <p className='font-bold'>LetMeKnow</p>
+              <div className='mr-1 ml-4 text-right'>
+                <div>
+                  <span className=''>Bought for ${data.transactionFee}</span>
+                </div>
+                <div>
+                  <span className=''>{formatDate(data.date)}</span>
+                </div>
               </div>
             </div>
-            <div className='mr-1 ml-4 text-right'>
-              <div>
-                <span className=''>Bought for $12</span>
-              </div>
-              <div>
-                <span className=''>Jan 20, 2024</span>
-              </div>
-            </div>
-          </div>
+          )}
           <div className='flex justify-center'>
             <div className='mt-2 hover:text-green-300 transition-all duration-200 flex items-center'>
               <span className='mr-1'>
-                <RiRepeatFill />
+                <RiRepeatFill/>
               </span>
               <span className='font-bold'>Traded 1 time</span>
               <span className='ml-1'>
-                <FaAngleRight />
+                <FaAngleRight/>
               </span>
             </div>
           </div>
         </div>
       </DialogTrigger>
       <DialogContent>
-        <ActivityLog />
+        <ActivityLog/>
       </DialogContent>
     </Dialog>
   )
